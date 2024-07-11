fa440952a8866316f5228f4519b6b0e0cf63928f
haodev1202 <admin@Tran-Van-Hao.local>
2024-07-11 11:00:46 +0700

    UseEffect

--- src/App.jsx
+++ src/App.jsx
@@ -11,14 +11,33 @@ function App() {
     );
 
     // useEffect
+    // Xử lí thực hiện các side-effects
+    // Side effects là chương trình phần mềm khi có một tác động xảy ra thì dữ liệu thay đổi
+    // update dom , dom api ,...
+
+    // Được gọi mỗi khi component re-render
+    // Gọi callback sau khi thêm element vào dom
+    useEffect(() => {
+        console.log("2");
+    });
+
+    // Gọi callback một lần sau khi component được mounted
     useEffect(() => {
         setFullName({
             name: "Tran Van Hao",
             familyName:
-                "Hoang gia, Cam vu, Cam giang, Hai duong",
+                "Hoang Gia Cam Vu Cam Giang Hai Duong",
         });
-    });
+        console.log(3);
+    }, []);
 
+    // Gọi callback sau khi deps thay đổi
+    useEffect(() => {
+        console.log(4);
+    } , [fullName])
+
+
+    console.log("Tran van hao");
     return (
         <>
             <div>
