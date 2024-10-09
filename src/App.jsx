fbe3aaaf796390ceb54ffeef9efd1825ab00e654
haodev1202 <haodev1202@gmail.com>
2024-10-09 19:17:19 +0700

    test commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,19 @@
 import { useState, useEffect } from "react";
 
 function App() {
-    
-
     return (
         <>
             <h1>Khái niệm SPA/MPA</h1>
             <p>
-                Single page aplication and Multi page aplication
-                Ứng dụng trang đơn và Ứng dụng nhiều trang
-
-                - Ứng dụng trang đơn hoạt động trong một phiên trình duyệt
-                và cho người dùng chuyển đổi giữa các phần của hệ thống
-                mà không cần phải tải lại toàn bộ trang
-                - Ứng dụng nhiều trang yêu cầu phải tải lại trang mỗi khi người
-                dùng tương tác với ứng dụng
-
-                SPA thường nhanh hơn cung cấp trải nghiệm người dùng liền mạch hơn
-                MPA thường phù hợp với nội dung nhất định  
+                Single page aplication and Multi page aplication Ứng dụng trang
+                đơn và Ứng dụng nhiều trang - Ứng dụng trang đơn hoạt động trong
+                một phiên trình duyệt và cho người dùng chuyển đổi giữa các phần
+                của hệ thống mà không cần phải tải lại toàn bộ trang - Ứng dụng
+                nhiều trang yêu cầu phải tải lại trang mỗi khi người dùng tương
+                tác với ứng dụng SPA thường nhanh hơn cung cấp trải nghiệm người
+                dùng liền mạch hơn MPA thường phù hợp với nội dung nhất định
             </p>
+            <p>Testcommit</p>
         </>
     );
 }
